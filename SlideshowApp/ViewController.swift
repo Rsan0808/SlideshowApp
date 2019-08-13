4b9a52852f5c21636c1e92a474fd67e2399cc993
Rsan <rsandane@gmail.com>
2019-08-13 14:46:23 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Rsan on 2019/08/13.
+//  Copyright © 2019 Rsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
